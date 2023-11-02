ce6592546ba9bc328bc52956ac2af5e35b820d0a
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-11-03 00:20:14 +0100

    i just made the svg icon

--- src/pages/FullScreenBackground.jsx
+++ src/pages/FullScreenBackground.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FullScreenBackground = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url("../src/images/roman heaven.jpg")' }}>
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url("../src/assets/images/roman heaven.jpg")' }}>
       {/* Your content goes here */}
     </div>
   );
