35c8515e05b94957e111485631c32d8dfecd20eb
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-11-02 19:59:49 +0100

    i add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import FullScreenBackground from './FullScreenBackground';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div className="App">
+      <FullScreenBackground />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 class="italic text-8xl absolute bottom-60 left-20">ADNANE BENSOUDA</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/FullScreenBackground.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FullScreenBackground = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url("../src/images/roman heaven.jpg")' }}>
+      {/* Your content goes here */}
+    </div>
+  );
+};
+
+export default FullScreenBackground;
