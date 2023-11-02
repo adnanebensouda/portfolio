8e2d113209289d2f05cc3712f52217d55b9cf135
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-11-02 22:17:59 +0100

    i just make folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import FullScreenBackground from './FullScreenBackground';
+import FullScreenBackground from './pages/FullScreenBackground';
 
 function App() {
   return (
