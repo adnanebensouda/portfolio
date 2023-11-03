aee26e8a2738cab409b2f8f4ec227c0f0b078bd9
adnanebensouda <bensouda.adnane.dev@gmail.com>
2023-11-03 12:00:32 +0100

    good structer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import FullScreenBackground from './pages/FullScreenBackground';
+import FullScreenBackground from './components/FullScreenBackground';
 
 function App() {
   return (
     <div className="App">
       <FullScreenBackground />
-      <div>
-        <h1 class="italic text-8xl absolute bottom-60 left-20">ADNANE BENSOUDA</h1>
-      </div>
+      
     </div>
   );
 }
